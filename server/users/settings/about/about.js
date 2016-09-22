3b32b58266144898e88a846f7e16a16ce0b4d1aa
Alex Kocharin <alex@kocharin.ru>
2016-09-22 17:29:54 +0000

    Make firstname, lastname and birthday immutable

--- server/users/settings/about/about.js
+++ server/users/settings/about/about.js
@@ -6,17 +6,27 @@ module.exports = function (N, apiPath) {
 
   N.wire.after(apiPath, { priority: 5 }, function add_fields(env) {
 
+    let is_name_set = !!(env.data.user.first_name || env.data.user.last_name);
+
     env.res.about.push({
       name:     'first_name',
       value:    env.data.user.first_name,
+      readonly: is_name_set,
       priority: 4
     });
 
     env.res.about.push({
       name:     'last_name',
       value:    env.data.user.last_name,
+      readonly: is_name_set,
       priority: 5
     });
 
+    // make birthday read-only if set
+    env.res.about.forEach(field => {
+      if (field.name === 'birthday') {
+        field.readonly = !!field.value;
+      }
+    });
   });
 };
--- server/users/settings/about/update.js
+++ server/users/settings/about/update.js
@@ -13,15 +13,40 @@ const validate = require('is-my-json-valid')({
 
 module.exports = function (N, apiPath) {
 
-  N.wire.before(apiPath, { priority: -5 }, function update_user(env) {
+  N.wire.before(apiPath, { priority: -20 }, function update_user(env) {
     if (!validate(env.params)) throw N.io.BAD_REQUEST;
 
-    if (env.data.user.first_name !== env.params.first_name) {
-      env.data.user.first_name = env.params.first_name;
+    env.data.user.about = env.data.user.about || {};
+    env.data.errors     = env.data.errors || {};
+    env.res.fields      = env.res.fields  || {};
+
+    if (env.data.user.first_name || env.data.user.last_name) {
+      // if name is already set, can't change it
+      if (env.data.user.first_name !== env.params.first_name ||
+          env.data.user.last_name  !== env.params.last_name) {
+
+        env.data.errors.first_name = true;
+        env.data.errors.last_name  = true;
+      }
+    } else if (env.params.first_name || env.params.last_name) {
+      // if user is trying to set a name, make sure both parts are non-empty
+      if (!env.params.first_name) env.data.errors.first_name = true;
+      if (!env.params.last_name)  env.data.errors.last_name  = true;
+
+      if (env.data.user.first_name !== env.params.first_name) {
+        env.data.user.first_name = env.params.first_name;
+      }
+
+      if (env.data.user.last_name !== env.params.last_name) {
+        env.data.user.last_name  = env.params.last_name;
+      }
     }
 
-    if (env.data.user.last_name !== env.params.last_name) {
-      env.data.user.last_name = env.params.last_name;
+    // make sure birthday cannot be changed once set
+    if (env.data.user.about.birthday) {
+      if (env.data.user.about.birthday.toISOString().slice(0, 10) !== env.params.birthday) {
+        env.data.errors.birthday = true;
+      }
     }
   });
 
@@ -30,7 +55,13 @@ module.exports = function (N, apiPath) {
   // so we need to fetch normalized values after that happens
   //
   N.wire.after(apiPath, { priority: 20 }, function fill_normalized_values(env) {
-    env.res.fields.first_name = env.data.user.first_name;
-    env.res.fields.last_name  = env.data.user.last_name;
+    let is_name_set = !!(env.data.user.first_name || env.data.user.last_name);
+
+    env.res.fields.first_name = { value: env.data.user.first_name, readonly: is_name_set };
+    env.res.fields.last_name  = { value: env.data.user.last_name, readonly: is_name_set };
+
+    if (env.res.fields.birthday) {
+      env.res.fields.birthday.readonly = !!env.res.fields.birthday.value;
+    }
   });
 };
