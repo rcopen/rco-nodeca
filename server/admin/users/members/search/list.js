43b7f2bbace6a98175abbab2028b55cccac2a9c2
Alex Kocharin <alex@kocharin.ru>
2017-01-12 12:04:41 +0000

    Use env.params.$query in members search in ACP

--- server/admin/users/members/search/list.js
+++ server/admin/users/members/search/list.js
@@ -15,16 +15,17 @@ const validate = require('is-my-json-valid')({
 
 module.exports = function (N, apiPath) {
   N.wire.before(apiPath, function create_search_query(env) {
-    if (!validate(env.params)) throw N.io.BAD_REQUEST;
+    if (!validate(env.data.search_params)) throw N.io.BAD_REQUEST;
 
     let search_query = env.data.search_query = env.data.search_query || {};
+    let search_params = env.data.search_params;
 
-    if (env.params.first_name) {
-      search_query.first_name_lc = new RegExp('^' + _.escapeRegExp(env.params.first_name.toLowerCase()));
+    if (search_params.first_name) {
+      search_query.first_name_lc = new RegExp('^' + _.escapeRegExp(search_params.first_name.toLowerCase()));
     }
 
-    if (env.params.last_name) {
-      search_query.last_name_lc = new RegExp('^' + _.escapeRegExp(env.params.last_name.toLowerCase()));
+    if (search_params.last_name) {
+      search_query.last_name_lc = new RegExp('^' + _.escapeRegExp(search_params.last_name.toLowerCase()));
     }
   });
 };
