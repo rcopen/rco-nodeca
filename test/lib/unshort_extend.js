5fad2c31d9fdb5f7c74b4cac2005e35c3e2f6997
Vitaly Puzrin <vitaly@rcdesign.ru>
2017-02-10 11:19:24 +0300

    Refactor links cleanup + tests

--- test/lib/unshort_extend.js
+++ test/lib/unshort_extend.js
@@ -1,11 +1,13 @@
-
+// Test additional unshorteners
+//
 'use strict';
 
 
 const assert  = require('assert');
+const Promise = require('bluebird');
 
 
-describe.only('Additional shorteners', function () {
+describe('Additional shorteners', function () {
 
   before(function () {
     if (!process.env.LINKS_CHECK) this.skip();
@@ -13,17 +15,27 @@ describe.only('Additional shorteners', function () {
 
 
   /* eslint-disable max-len */
-  it('Unshort ali.pub', function () {
+  it('Unshort aliexpress', Promise.coroutine(function* () {
     let url = 'http://ali.pub/y9fu8';
     let data = { url, types: [ 'inline' ] };
 
-    return TEST.N.wire.emit('internal:common.embed', data).then(() => {
-      assert.strictEqual(
-        data.canonical,
-        'https://www.aliexpress.com/item/Electric-motor-shaft-Mini-Chuck-Fixture-Clamp-0-3mm-3-5mm-Small-To-Drill-Bit-Micro/32706864811.html'
-      );
-    });
-  });
+    yield TEST.N.wire.emit('internal:common.embed', data);
+
+    assert.strictEqual(
+      data.canonical,
+      'https://www.aliexpress.com/item/Electric-motor-shaft-Mini-Chuck-Fixture-Clamp-0-3mm-3-5mm-Small-To-Drill-Bit-Micro/32706864811.html'
+    );
+
+    url = 'http://s.aliexpress.com/ZjIRFZnQ';
+    data = { url, types: [ 'inline' ] };
+
+    yield TEST.N.wire.emit('internal:common.embed', data);
+
+    assert.strictEqual(
+      data.canonical,
+      'http://www.aliexpress.com/item/32744783165/32744783165.html'
+    );
+  }));
 
 
   it('Unshort banggood.app.link', function () {
