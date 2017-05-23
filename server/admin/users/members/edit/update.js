9c264a355ec9cb512d1d6b1fd812bb2cb27fa28c
Alex Kocharin <alex@kocharin.ru>
2017-05-23 17:19:23 +0000

    Implement moderator notes

--- server/admin/users/members/edit/update.js
+++ server/admin/users/members/edit/update.js
@@ -16,11 +16,11 @@ module.exports = function (N, apiPath) {
   N.wire.before(apiPath, { priority: -5 }, function update_user(env) {
     if (!validate(env.params)) throw N.io.BAD_REQUEST;
 
-    if (env.data.user.first_name !== env.params.first_name) {
+    if (typeof env.params.first_name !== 'undefined' && env.params.first_name !== env.data.user.first_name) {
       env.data.user.first_name = env.params.first_name;
     }
 
-    if (env.data.user.last_name !== env.params.last_name) {
+    if (typeof env.params.last_name !== 'undefined' && env.params.last_name !== env.data.user.last_name) {
       env.data.user.last_name = env.params.last_name;
     }
 
